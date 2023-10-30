4882adb28b6451a3b2903596a550b2bc75356762
jeffrey <jeffrey913@outlook.com>
2023-10-30 16:27:08 -0700

    upload solution

---
+++ Beginner/1016/1016.cpp
@@ -0,0 +1,24 @@
+#pragma gcc optimizer   ("O3")
+#pragma gcc target      ("sse4")
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+	int t,n;
+    double avg;
+    cin>>t;
+    while(t--){
+        avg = 0;
+        int a=0;
+        cin>>n;
+        double tn;
+		for(int i=0;i<n;++i){	
+            cin >> tn;
+            avg += tn;
+            a += (tn >= 90.00);
+		}
+        cout << avg / n << " " << a << "\n"; 
+    }
+}
